4e90d53a40abba11f2fc3a759a9cebe4f1919abe
Ken Gaillot <kg@boogieonline.com>
2014-09-14 18:08:18 -0400

    retab source files using 4-space tabs

--- rump.c
+++ rump.c
@@ -1,9 +1,9 @@
 /*
-	rump.c
+    rump.c
 
-	library to parse Rudimentary Markup 
+    library to parse Rudimentary Markup 
 
-	Copyright (c)2014 Ken Gaillot <kg@boogieonline.com>
+    Copyright (c)2014 Ken Gaillot <kg@boogieonline.com>
 */
 
 #include <stdio.h>
@@ -11,109 +11,109 @@
 #include "rump.h"
 
 /*
-	language definition functions
+    language definition functions
 
-	A more sophisticated library would define the language via DTD or some
-	such, but rump uses a function (rum_tag_insert()) to build the language
-	description tag by tag.
+    A more sophisticated library would define the language via DTD or some
+    such, but rump uses a function (rum_tag_insert()) to build the language
+    description tag by tag.
 
-	A more sophisticated library would also have better error handling and
-	reporting.  Most rump functions simply return NULL on error with no
-	explanation.
+    A more sophisticated library would also have better error handling and
+    reporting.  Most rump functions simply return NULL on error with no
+    explanation.
 */
 
 rum_tag_t *
 rum_tag_insert(rum_tag_t *parent, const char *name, int is_empty, rum_attr_t *attrs)
 {
-	rum_tag_t *tag;
+    rum_tag_t *tag;
 
-	/* can't add a child tag to an empty tag */
-	if (parent && parent->is_empty) {
-		return NULL;
-	}
+    /* can't add a child tag to an empty tag */
+    if (parent && parent->is_empty) {
+        return NULL;
+    }
 
-	/* create a tag instance
-	 * (in the interest of brevity, no special memory management is done,
-	 * just a bunch of small mallocs) */
-	if ((tag = malloc(sizeof(rum_tag_t))) == NULL) {
-		return NULL;
-	}
-	tag->name = name;
-	tag->parent = parent;
-	tag->is_empty = is_empty;
+    /* create a tag instance
+     * (in the interest of brevity, no special memory management is done,
+     * just a bunch of small mallocs) */
+    if ((tag = malloc(sizeof(rum_tag_t))) == NULL) {
+        return NULL;
+    }
+    tag->name = name;
+    tag->parent = parent;
+    tag->is_empty = is_empty;
 
-	/* for simplicity, the attrs argument is required to point to static
-	 * memory; a fuller implementation would malloc a copy here */
-	tag->attrs = attrs;
+    /* for simplicity, the attrs argument is required to point to static
+     * memory; a fuller implementation would malloc a copy here */
+    tag->attrs = attrs;
 
-	/* a fuller implementation could do further validation here,
-	 * such as ensuring that a tag by this name is not already defined */
+    /* a fuller implementation could do further validation here,
+     * such as ensuring that a tag by this name is not already defined */
 
-	/* add this tag to the tree */
-	if (parent) {
-		if (parent->first_child == NULL) {
-			parent->first_child = tag;
-		} else {
-			rum_tag_t *child = parent->first_child;
-			while (child->next_sibling != NULL)
-				child = child->next_sibling;
-			child->next_sibling = tag;
-		}
-	}
-	return tag;
+    /* add this tag to the tree */
+    if (parent) {
+        if (parent->first_child == NULL) {
+            parent->first_child = tag;
+        } else {
+            rum_tag_t *child = parent->first_child;
+            while (child->next_sibling != NULL)
+                child = child->next_sibling;
+            child->next_sibling = tag;
+        }
+    }
+    return tag;
 }
 
 rum_tag_t *
 rum_tag_get_parent(const rum_tag_t *tag)
 {
-	return tag? tag->parent : NULL;
+    return tag? tag->parent : NULL;
 }
 
 rum_tag_t *
 rum_tag_get_next_sibling(const rum_tag_t *tag)
 {
-	return tag? tag->next_sibling : NULL;
+    return tag? tag->next_sibling : NULL;
 }
 
 rum_tag_t *
 rum_tag_get_first_child(const rum_tag_t *tag)
 {
-	return tag? tag->first_child : NULL;
+    return tag? tag->first_child : NULL;
 }
 
 static void
 rum_display_language_subtree(const rum_tag_t *root, int indent_level)
 {
-	const rum_tag_t *tag = root;
-	const rum_attr_t *attr = NULL;
+    const rum_tag_t *tag = root;
+    const rum_attr_t *attr = NULL;
 
-	while (tag != NULL) {
-		printf("%*sTAG %s (%s)\n", (indent_level * 3), " ", tag->name,
-			(tag->is_empty? "empty": "nonempty"));
-		for (attr = tag->attrs; attr && attr->name; ++attr) {
-			printf("%*sATTR %s (%s)\n", (indent_level * 3), " ", attr->name,
-				(attr->is_required? "required" : "optional"));
-		}
-		printf("\n");
-		if (tag->first_child) {
-			rum_display_language_subtree(tag->first_child, indent_level + 1);
-		}
-		tag = tag->next_sibling;
-	}
+    while (tag != NULL) {
+        printf("%*sTAG %s (%s)\n", (indent_level * 3), " ", tag->name,
+            (tag->is_empty? "empty": "nonempty"));
+        for (attr = tag->attrs; attr && attr->name; ++attr) {
+            printf("%*sATTR %s (%s)\n", (indent_level * 3), " ", attr->name,
+                (attr->is_required? "required" : "optional"));
+        }
+        printf("\n");
+        if (tag->first_child) {
+            rum_display_language_subtree(tag->first_child, indent_level + 1);
+        }
+        tag = tag->next_sibling;
+    }
 }
 
 void
 rum_display_language(const rum_tag_t *root)
 {
-	if (root == NULL) {
-		printf("The language is undefined.\n\n");
-	} else {
-		printf("The language consists of these tags and attributes:\n\n");
-		rum_display_language_subtree(root, 0);
-	}
+    if (root == NULL) {
+        printf("The language is undefined.\n\n");
+    } else {
+        printf("The language consists of these tags and attributes:\n\n");
+        rum_display_language_subtree(root, 0);
+    }
 }
 
 /*
-	language parsing functions
-	(TBD)
+    language parsing functions
+    (TBD)
 */
--- rump.h
+++ rump.h
@@ -1,40 +1,40 @@
 /*
-	rump.h
+    rump.h
 
-	header for RuM parser library
+    header for RuM parser library
 
-	Copyright (c)2014 Ken Gaillot <kg@boogieonline.com>
+    Copyright (c)2014 Ken Gaillot <kg@boogieonline.com>
 */
 
 /*
-	Declarations for language definition
+    Declarations for language definition
 */
 
 /* XML attribute: name="value" pair */
 typedef struct rum_attr_s {
-	/* this attribute's name */
-	const char *name;
+    /* this attribute's name */
+    const char *name;
 
-	/* whether this attribute is required to be present in its tag (boolean) */
-	int is_required;
+    /* whether this attribute is required to be present in its tag (boolean) */
+    int is_required;
 } rum_attr_t;
 
 /* XML tag: <tag [attrs] /> if empty, <tag [attrs]> ... </tag> otherwise */
 typedef struct rum_tag_s {
-	/* this tag's place in the language's tag tree;
-	 * parent is NULL if this is the root tag, otherwise this tag is only valid within parent */
-	struct rum_tag_s *parent;
-	struct rum_tag_s *next_sibling;
-	struct rum_tag_s *first_child;
+    /* this tag's place in the language's tag tree;
+     * parent is NULL if this is the root tag, otherwise this tag is only valid within parent */
+    struct rum_tag_s *parent;
+    struct rum_tag_s *next_sibling;
+    struct rum_tag_s *first_child;
 
-	/* the tag itself */
-	const char *name;
+    /* the tag itself */
+    const char *name;
 
-	/* whether this is an empty tag (boolean) */
-	int is_empty;
+    /* whether this is an empty tag (boolean) */
+    int is_empty;
 
-	/* list of attributes accepted by this tag (null attribute name indicates end of list) */
-	rum_attr_t *attrs;
+    /* list of attributes accepted by this tag (null attribute name indicates end of list) */
+    rum_attr_t *attrs;
 } rum_tag_t;
 
 /* a language definition is simply a pointer to the root tag */
@@ -46,24 +46,24 @@ rum_tag_t *rum_tag_get_first_child(const rum_tag_t *tag);
 void rum_display_language(const rum_tag_t *root);
 
 /*
-	Declarations for language parsing
-	(TBD)
+    Declarations for language parsing
+    (TBD)
 */
 
 /* XML element: a tag instance and its associated attribute values and content */
 typedef struct rum_element_s {
-	/* this tag's place in the document's tag tree;
-	 * parent is NULL if this is the root tag, otherwise this tag is contained within parent */
-	struct rum_element_s *parent;
-	struct rum_element_s *next_sibling;
-	struct rum_element_s *first_child;
+    /* this tag's place in the document's tag tree;
+     * parent is NULL if this is the root tag, otherwise this tag is contained within parent */
+    struct rum_element_s *parent;
+    struct rum_element_s *next_sibling;
+    struct rum_element_s *first_child;
 
-	/* tag that this element is an instance of */
-	const rum_tag_t *tag;
+    /* tag that this element is an instance of */
+    const rum_tag_t *tag;
 
-	/* a list of attribute name-value pairs; null name indicates end of list */
-	char **attrs;
+    /* a list of attribute name-value pairs; null name indicates end of list */
+    char **attrs;
 
-	/* this element's content (NULL for empty tags) */
-	char *content;
+    /* this element's content (NULL for empty tags) */
+    char *content;
 } rum_element_t;
